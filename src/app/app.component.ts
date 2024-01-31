9508bb5e5085e87c34860d9d503f88230e604a31
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-01-31 21:39:10 +0700

    reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,16 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ListcontentComponent } from './listcontent/listcontent.component';
 import { FormSectionComponent } from './form-section/form-section.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,ListcontentComponent,FormSectionComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,ListcontentComponent,FormSectionComponent,ReactiveFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Hello WOrld';
-  nama = "Adi Kurniawan"
+  nama = "Adi Kurniawan";
 }
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
-
+  name = new FormControl('')
 }
